6aec0da74bcf84de8a2b87e9f9ff0940f99fba39
Matias Bertani <matiasbertani@gmail.com>
2024-07-15 00:53:08 -0300

    add main module to glue evrything

---
+++ main.py
@@ -0,0 +1,17 @@
+from final_report import FinalReport
+from management_report import ManagementReport
+from nx_portfolio import NxPortfolio
+from pre_transformed_report import PreTransformedReport
+
+
+if __name__ == "__main__":
+
+    management_report = ManagementReport()
+    nx_portfolio = NxPortfolio()
+
+    transformed_report = PreTransformedReport(management_report, nx_portfolio).transform()
+
+    final_report = FinalReport(transformed_report)
+    final_report.generate()
+    final_report.validate()
+    final_report.save()
