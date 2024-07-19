0bd83e719f5ccd4282100eb370f0fac6809b28eb
Matias Bertani <matiasbertani@gmail.com>
2024-07-19 19:26:18 -0300

    add constants module to declare the directories as contants

---
+++ constants.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+ROOT_PATH = Path(__file__).resolve().parent
+DATABASE_PATH = ROOT_PATH / "database"
+INPUT_PATH = ROOT_PATH / "input"
+RESULTS_PATH = ROOT_PATH / "results"
+VERIFICATION_PATH = ROOT_PATH / "verification"
+
+
+if __name__ == "__main__":
+    print("The root of the project is:")
+    print(ROOT_PATH)
